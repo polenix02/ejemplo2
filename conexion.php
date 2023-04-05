0285616f6a3cea67855158a49d8f9747a8d24973
Paul <pol99954@gmail.com>
2023-04-05 16:13:02 -0500

    V2

--- conexion.php
+++ conexion.php
@@ -1,10 +1,10 @@
 <?php
 	function conexion(){
-	$host = "host=containers-us-west-38.railway.app";
-	$port = "port=8029";
+	$host = "host=containers-us-west-156.railway.app";
+	$port = "port=7704";
 	$dbname = "dbname=railway";
 	$user = "user=postgres";
-	$password = "password=JIcL9swq3Y5O3TS911yU";
+	$password = "password=XWpe9oXWa2BhobUxnnhH";
 	$db = pg_connect("$host $port $dbname $user $password");
     return $db;
 }
